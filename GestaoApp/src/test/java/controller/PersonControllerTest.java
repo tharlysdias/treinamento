ef802042c0c03e54fa3f0ac53e3675444d0ca7bb
Tharlys Dias <tharlys.souza@outlook.com>
2021-02-25 16:03:59 -0300

    Iniciando testes com junit

---
+++ GestaoApp/src/test/java/controller/PersonControllerTest.java
@@ -0,0 +1,21 @@
+package controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PersonControllerTest {
+
+	@Test
+	public void testSavePerson() {
+		PersonControllerTest personControl = new PersonControllerTest();
+		boolean opLogico = personControl.equals(personControl);
+		assertEquals(true, opLogico);
+	}
+
+	@Test
+	public void testGetPersons() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ GestaoApp/src/test/java/util/FileTest.java
@@ -0,0 +1,14 @@
+package util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FileTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
